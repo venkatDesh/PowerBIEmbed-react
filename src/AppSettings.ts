294eceb06bc24b994fed184187be6a7e9ce32bda
venkatesh <36484375+venkatDesh@users.noreply.github.com>
2022-08-17 11:55:35 +0530

    Add files via upload

---
+++ src/AppSettings.ts
@@ -0,0 +1,5 @@
+export default class AppSettings {
+  static aadTenant: string = "htcinc.com";
+  static clientId: string = "94c4c2ad-0f43-4bf5-829e-9866802f3bdb";
+  static appWorkspaceId: string = "f2f4b9b8-709d-46f8-b972-956038e359a7";
+}
